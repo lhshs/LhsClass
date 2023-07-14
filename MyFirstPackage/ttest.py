4ff10f88fde1e81bdee0ae13ebc37f9783d8f387
lhshs <hs.lion@gmail.com>
2023-07-14 16:15:05 +0900

    package error fix

--- MyFirstPackage/ttest.py
+++ MyFirstPackage/ttest.py
@@ -3,6 +3,16 @@
 # value : y axis column
 # group1, group2 : col (x column) 기준으로 분류된 이름
 
+import seaborn as sns
+from statannot import add_stat_annotation
+import matplotlib.pyplot as plt
+plt.rc('font', family='Malgun Gothic')  
+plt.rcParams['axes.unicode_minus'] = False
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
 def ttest_auto(user_df, col, value, group1, group2):
     f1 = sns.boxplot(data=user_df, x=col, y = value, palette='pastel')
     f2 = sns.stripplot(data=user_df, x=col, y = value, alpha=.5, color='r')
